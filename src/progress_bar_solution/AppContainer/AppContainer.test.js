85ad2d59637b33a528198d1e4feaf8de4fc03f5a
Matt Pickett <39273362+Pickett-Matthew@users.noreply.github.com>
2021-08-03 23:23:27 -0600

    notes where I would improve'

--- src/progress_bar_solution/AppContainer/AppContainer.test.js
+++ src/progress_bar_solution/AppContainer/AppContainer.test.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDom from "react-dom";
 import AppContainer from "./AppContainer";
 
+// if I had more time I would write better unit tests!!
 it("AppContainer renders on the page", () => {
   const testEle = document.createElement("DIV");
   ReactDom.render(<AppContainer />, testEle);
